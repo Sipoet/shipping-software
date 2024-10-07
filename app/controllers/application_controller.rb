b1e7d7dc065dceb94f2b42ffde14ff80ca01c78e
reidy <slayoslay@gmail.com>
2024-10-07 12:20:24 +0700

    init theme js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  include BreadcrumbGenerator
+  include TableSearchParamsExtractor
+
+  before_action :init_main_breadcrumbs, :set_locale
+  # allow_browser versions: :modern
+
+  private
+  def init_main_breadcrumbs
+    init_breadcrumb
+    add_breadcrumb(root_path, 'Dashboard')
+  end
+
+  def set_locale
+    I18n.locale = :id
+  end
 end
